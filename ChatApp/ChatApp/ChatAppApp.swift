6efb6070f5860d556319505606061756199f8a65
Mihael Mihaylov <mishoni04@abv.bg>
2024-12-26 23:15:36 +0200

    init
    first_commit

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Mishoni Mihaylov on 26.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
